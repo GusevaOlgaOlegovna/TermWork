c441300b726894d5c68402de321a70ea4514af00
Гусева Ольга Олеговна <guseva.olga.vua@mail.ru>
2023-03-28 19:13:46 +0300

    Разработали процедуру

---
+++ SQL Scripts/DBTermWork/PROC/Proc.sql
@@ -0,0 +1,18 @@
+﻿--Процедура (TopTypes) Топ видов (просмотр самых популярных путёвок по виду отдыха):
+
+CREATE PROCEDURE TopTypes AS
+BEGIN
+SELECT * FROM
+(
+	SELECT Type, COUNT(*) AS TripsCount
+	FROM Trip,Type WHERE Trip.ID_Type=Type.ID_Type
+	GROUP BY Trip.ID_Type, Type
+)AS counts
+WHERE TripsCount=
+(
+	SELECT TOP 1 COUNT(*)
+	FROM Trip
+	GROUP BY ID_Type
+	ORDER BY COUNT(*)DESC
+)
+END;
