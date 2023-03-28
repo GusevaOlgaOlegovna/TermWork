ea80526a95eea0707d99a8255a631fed23a2260f
Гусева Ольга Олеговна <guseva.olga.vua@mail.ru>
2023-03-28 19:23:18 +0300

    Разработали триггер

---
+++ SQL Scripts/DBTermWork/TRIGGERS/Trigger.sql
@@ -0,0 +1,7 @@
+﻿--Триггер (PreventDropTable) предотвращает удаление таблиц из БД:
+
+CREATE TRIGGER PreventDropTable
+ON DATABASE FOR DROP_TABLE
+AS
+	RAISERROR(N'Попытка удаления таблицы', 10, 1);
+	ROLLBACK;
