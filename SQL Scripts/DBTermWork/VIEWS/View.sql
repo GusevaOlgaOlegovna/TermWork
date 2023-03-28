df7465b089071206ad55fef31de9a439ef97cbbe
Гусева Ольга Олеговна <guseva.olga.vua@mail.ru>
2023-03-28 19:36:04 +0300

    Разработали представления

---
+++ SQL Scripts/DBTermWork/VIEWS/View.sql
@@ -0,0 +1,9 @@
+﻿--Представление (Countries) Страны (просмотр всех стран, в которые есть путёвки):
+
+CREATE VIEW Countries AS
+SELECT Country FROM Country;
+
+--Представление (Cities) Города (просмотр всех городов, в которые есть путёвки):
+
+CREATE VIEW Cities AS
+SELECT City FROM City;
