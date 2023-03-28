9e119703a24fcbf5d32ad0415a05e8266c83e4a2
Гусева Ольга Олеговна <guseva.olga.vua@mail.ru>
2023-03-28 18:36:40 +0300

    Создали базу данных TravelAgency и таблицы

---
+++ SQL Scripts/DBTermWork/CREATE/Create.sql
@@ -0,0 +1,44 @@
+﻿--Создание базы данных «TravelAgency»:
+
+CREATE DATABASE TravelAgency;
+
+--Создание таблицы Страны:
+CREATE TABLE Country
+(
+	  ID_Country INT PRIMARY KEY
+	, Country NVARCHAR(30) NOT NULL UNIQUE
+);
+
+--Создание таблицы Города:
+CREATE TABLE City
+(
+	  ID_City INT PRIMARY KEY
+    , ID_Country INT FOREIGN KEY REFERENCES Country(ID_Country)
+	, City NVARCHAR(30) NOT NULL UNIQUE
+);
+
+--Создание таблицы Типы отдыха:
+CREATE TABLE [Type]
+(
+	  ID_Type INT PRIMARY KEY
+	, Type NVARCHAR(30) NOT NULL UNIQUE
+);
+
+--Создание таблицы Клиенты:
+CREATE TABLE Person
+(
+	  ID INT PRIMARY KEY
+	, FullName NVARCHAR(100) NOT NULL
+);
+
+--Создание таблицы Путёвки:
+CREATE TABLE Trip
+(
+	  ID_Trip INT PRIMARY KEY
+	, ID_Country INT FOREIGN KEY REFERENCES Country(ID_Country)
+	, ID_City INT FOREIGN KEY REFERENCES City (ID_City)
+	, ID_Type INT FOREIGN KEY REFERENCES Type (ID_Type)
+	, ID_Person INT FOREIGN KEY REFERENCES Person (ID)
+	, Price DECIMAL
+	, [Data] DATE
+);
